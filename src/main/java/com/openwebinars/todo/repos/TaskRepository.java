269f28ee799c0f27b58073af047bfac861efd2c8
therki <123902898+therki@users.noreply.github.com>
2026-05-12 20:35:32 +0200

    Creacion repos

--- src/main/java/com/openwebinars/todo/repos/TaskRepository.java
+++ src/main/java/com/openwebinars/todo/repos/TaskRepository.java
@@ -1,13 +1,45 @@
 package com.openwebinars.todo.repos;
 
+import com.openwebinars.todo.model.Category;
 import com.openwebinars.todo.model.Task;
 import com.openwebinars.todo.users.User;
+import jakarta.persistence.EnumType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    /* Buscar por autor - todas las tareas del usuario*/
     List<Task> findByAuthor(User author);
+
+    /* Buscar por categoria */
+    List<Task> findByCategoryAndAuthor(Category category, User author);
+    List<Task> findByCategoryIdAndAuthor(Long id, User author);
+    List<Task> findByCategoryId(Long id);
+
+    /* Buscar por descripcion */
+    List<Task> findByDescriptionContainingIgnoreCaseAndAuthor(String description, User author);
+    /* Buscar por título */
+    List<Task> findByTitleContainingIgnoreCaseAndAuthor(String title, User author);
+
+    /* Buscar por completadas */
+    List<Task> findByCompletedAndAuthor(boolean completed, User author);
+
+    /* Buscar por fecha de creacion */
+    List<Task> findByCreatedAtAndAuthor(LocalDateTime createdAt, User author);
+
+    /* Buscar por fecha limite */
+    List<Task> findByDeadlineLessThanEqualAndAuthor(LocalDate deadline, User author);
+
+    /* Buscar por nombre de etiqueta y autor */
+    List<Task> findByTags_NameIgnoreCaseAndAuthor(String tagName, User author);
+
+    /* Buscar por prioridad */
+    List<Task> findByPriorityAndAuthor(Task.Priority priority, User author);
 }
--- src/main/java/com/openwebinars/todo/users/UserRepository.java
+++ src/main/java/com/openwebinars/todo/users/UserRepository.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findFirstByUsername(String username);
-
+    Optional<User> findById(Long id);
+    Optional<User> findByEmail(String email);
 }
