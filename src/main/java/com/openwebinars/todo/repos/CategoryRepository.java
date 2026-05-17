7126a5642f0f0dfacecb66b224b230c4d2a570ca
therki <123902898+therki@users.noreply.github.com>
2026-05-17 19:37:39 +0200

    Actualizar servicio catgoria: buscar por titulo

--- src/main/java/com/openwebinars/todo/repos/CategoryRepository.java
+++ src/main/java/com/openwebinars/todo/repos/CategoryRepository.java
@@ -1,7 +1,12 @@
 package com.openwebinars.todo.repos;
 
 import com.openwebinars.todo.model.Category;
+import com.openwebinars.todo.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByTitle(String title);
+
 }
